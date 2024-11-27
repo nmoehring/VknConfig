33c26051f5515b739088e73dfa4ed37be575946e
Nathaniel Moehring <nathan.moehring@gmail.com>
2024-11-27 14:24:20 -0600

    VSCode now builds and debugs only the chapter associated with the open source file, and auto debugs the executable built from the open source file, and will just throw an error if you try it on the wrong file.

--- build_debug.bat
+++ build_debug.bat
@@ -1,2 +1,8 @@
-cmake . --preset debug
-cmake --build --preset debug
+
+IF "%1" == "" (
+    cmake . --preset debug
+    cmake --build --preset debug
+) ELSE (
+    cmake . --preset debug -DTARGET_CHAPTER=%1
+    cmake --build --preset debug -DTARGET_CHAPTER=%1
+)
