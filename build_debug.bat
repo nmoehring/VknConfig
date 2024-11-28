9661f4cc0819ab107d4e9af8a505827e12f2e96b
Nathaniel Moehring <nathan.moehring@gmail.com>
2024-11-28 16:53:59 -0600

    Added log options to launch configs and decided to do several different exploratory minimum build tests.

--- build_debug.bat
+++ build_debug.bat
@@ -1,8 +1,19 @@
+set vkckbkConfig=cmake . --preset debug
+set vkckbkBuild=cmake --build --preset debug
 
-IF "%1" == "" (
-    cmake . --preset debug -DTARGET_SUBDIR=ALL
-    cmake --build --preset debug
+IF "%1" == "-build-all" (
+    set vkckbkConfig=%vkckbkConfig% -DTARGET_SUBDIR=ALL
 ) ELSE (
-    cmake . --preset debug -DTARGET_SUBDIR=%1
-    cmake --build --preset debug 
+    set vkckbkConfig=%vkckbkConfig% -DTARGET_SUBDIR=%1
 )
+
+IF "%2" == "-log" (
+    set vkckbkConfig=%vkckbkConfig% > config.log
+    set vkckbkBuild=%vkckbkBuild% > build.log
+) ELSE IF "%2" == "-trace-log" (
+    set vkckbkConfig=%vkckbkConfig% --trace > config.log
+    set vkckbkBuild=%vkckbkBuild% --trace > build.log
+)
+
+%vkckbkConfig%
+%vkckbkBuild%
--- minTest/Direct-minTest.cpp
+++ minTest/Direct-minTest.cpp
@@ -4,18 +4,20 @@
 
 int main()
 {
+  // 1. CREATE INSTANCE
   VkInstance instance;
-  VkInstanceCreateInfo createInfo = {};
+  VkInstanceCreateInfo createInfo{};
   createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 
-  if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS)
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
   {
-    std::cout << "Failed to create Vulkan instance") << std::endl;
+    std::cout << "Failed to create Vulkan instance" << std::endl;
     return -1;
   }
 
+  // 2. SELECT PHYSICAL DEVICE
   VkPhysicalDevice physicalDevice{VK_NULL_HANDLE};
-  uint32_t deviceCount = 0;
+  uint32_t deviceCount{0};
   vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
   if (deviceCount == 0)
   {
@@ -27,16 +29,18 @@ int main()
   vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
   physicalDevice = devices[0];
 
+  // 3. CREATE DEVICE
   VkDevice device;
-  VkDeviceCreateInfo deviceCreateInfo = {};
+  VkDeviceCreateInfo deviceCreateInfo{};
   deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 
-  if (vkCreateDevice(physicalDevice, &deviceCreateInfo, NULL, &device) != VK_SUCCESS)
+  if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS)
   {
     std::cout << "Failed to create Vulkan device" << std::endl;
     return -1;
   }
 
+  // END: Destroy Vk stuff
   std::cout << "Vulkan device created successfully" << std::endl;
   vkDestroyDevice(device, NULL);
   vkDestroyInstance(instance, NULL);
